6acb51f1cb257b3e810e20ff2bf166c599814676
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-18 20:14:00 +0100

    Migrate tests to nested package pattern

--- Package.swift
+++ Package.swift
@@ -10,27 +10,23 @@ let package = Package(
         .iOS(.v26),
         .tvOS(.v26),
         .watchOS(.v26),
-        .visionOS(.v26),
+        .visionOS(.v26)
     ],
     products: [
-        .library(name: "W3C PNG", targets: ["W3C PNG"]),
+        .library(name: "W3C PNG", targets: ["W3C PNG"])
     ],
     dependencies: [
         .package(path: "../../swift-primitives/swift-standard-library-extensions"),
-        .package(path: "../swift-rfc-1950"),
+        .package(path: "../swift-rfc-1950")
     ],
     targets: [
         .target(
             name: "W3C PNG",
             dependencies: [
                 .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
-                .product(name: "RFC 1950", package: "swift-rfc-1950"),
+                .product(name: "RFC 1950", package: "swift-rfc-1950")
             ]
-        ),
-        .testTarget(
-            name: "W3C PNG".tests,
-            dependencies: ["W3C PNG"]
-        ),
+        )
     ],
     swiftLanguageModes: [.v6]
 )
@@ -43,6 +39,6 @@ for target in package.targets where ![.system, .binary, .plugin].contains(target
     target.swiftSettings = (target.swiftSettings ?? []) + [
         .enableUpcomingFeature("ExistentialAny"),
         .enableUpcomingFeature("InternalImportsByDefault"),
-        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("MemberImportVisibility")
     ]
 }
